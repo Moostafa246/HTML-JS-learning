1d70d8e08ee1cfb06ee3d9a1f6bf967e15485c0b
Mustafa Kazim <mustafa1.kazim@gmail.com>
2020-10-13 13:11:07 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>InExclusive</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h2>This is where it all starts</h2>
+    <hr>
+    <p id="top">Complete <b>it</b></p>
+
+    <ul>
+        <li><b>First</b></li>
+        <li><i>Second</i></li>
+        <li><sub>Third</sub></li>
+        <li><ins>Fourth</ins></li>
+
+    </ul>
+
+    <ol start="4">
+        <li><b>First</b></li>
+        <li><i>Second</i></li>
+        <li><sub>Third</sub></li>
+        <li><ins>Fourth</ins></li>
+    
+    </ol>
+    <ol type="i">
+        <li><b>First</b></li>
+        <li><i>Second</i></li>
+        <li><sub>Third</sub></li>
+        <li><ins>Fourth</ins></li>
+        
+    </ol>
+
+    <img src="syre.jpg" width="200" height="200" align="right">
+
+    <body>
+        <form>
+            <p>Search: <input type="text" placeholder="Write HERE"></p>
+            <p>Check: <input type="checkbox"></p>
+            <p>Please check yes or no: <p>yes<input type="radio" name="pick"> <p>no<input type="radio" name="pick"></p></p>
+
+            <p>Favourite Food:
+                <select>
+                    <option>Pizza</option>
+                    <option>Sandwich</option>
+                    <option>Fries</option>
+                    <option>Burgers</option>
+                </select>
+            </p>
+
+            <p><input type="submit" value="done."></p>
+
+        </form>
+        
+
+    </body>
+
+    <body>
+        <table>
+            <tr>
+                <td>me</td>
+                <td>you</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>6</td>
+             </tr>
+        </table>
+
+        <p>Shirt<a href="https://www2.hm.com/en_ca/productpage.0876069003.html"><img src="hm.jpeg" width="150" height ="200"></a></p>
+        <p>Pants<a href="https://www2.hm.com/en_ca/productpage.0714032017.html"><img src="pants.jpeg" width="150" height ="200"></a></p>
+        <p><a href="#top">Back to Top</a></p>
+    </body>
+    <body>
+        <body>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/lIlMtVGI5Pg" frameborder="0" allowfullscreen align="right"></iframe>
+    
+            <h1>Drake!</h1>
+            
+            <h2>The artist Drake is from my hometown Toronto!</h2>
+            
+            <img src="drake.jpeg" width="400">
+            
+            <p>Dont have much to say, just that he is the best rapper.</p>
+            
+            <hr>
+            
+            <h3>About Drake</h3>
+                <ul>
+                    <li>Records: Bare</li>
+                    <li>Streams: Bare</li>
+                    <li>Songs <a href="https://www.google.com/search?q=drake+songs&oq=drake+songs&aqs=chrome..69i57.1694j0j7&sourceid=chrome&ie=UTF-8">All of them are here!</a></li>
+                </ul>
+            
+            <hr>
+            
+                <h3>Top 3 facts about the Drake</h3>
+            
+                <ol>
+                    <li>sick</li>
+                    <li>Sick</li>
+                    <li>SICKK</li>
+                </ol>
+            <hr>
+            <table>
+            
+                <thead>
+                
+                    <tr>
+                    
+                        <th>Name</th>
+                        <th>Average Views</th>
+                        <th>Average<br>Awards</th>
+                        
+                    </tr>
+                    
+                </thead>
+                
+                <tbody>
+                
+                    <tr>
+                    
+                        <td>Drizz</td>
+                        <td>3,403m</td>
+                        <td>5</td>
+                        
+                    </tr>
+                    
+                    <tr>
+                    
+                        <td>6ixGod</td>
+                        <td>6,052m</td>
+                        <td>6</td>
+                        
+                    </tr>
+                
+                </tbody>
+            
+            </table>
+            
+            <hr>
+            
+            <form action="https://en.wikipedia.org/wiki/Drake_(musician)">
+            
+                <p>Want to know more?</p>
+                
+                <p><input type="submit" value="Click Here!"></p>
+            
+            </form>
+                
+    </body>
+
+
+</body>
+</html>
